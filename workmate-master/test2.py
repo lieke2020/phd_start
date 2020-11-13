d4401dc64606070c24a9ea0edef8dd7f4a315eea
Lieke Ceton <l.ceton@nin.knaw.nl>
2020-11-13 15:29:09 +0100

    Restoring jupyter notebooks

--- workmate-master/test2.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 12 16:54:45 2020
-
-@author: Lieke
-"""
-
-importlib control
--- workmate-master/testWM.py
+++ workmate-master/testWM.py
@@ -4,18 +4,8 @@ Created on Thu Nov 12 14:27:12 2020
 
 @author: Lieke
 """
-import w_runner
-from tictoc import tic, toc
 
-tic()
-#w_runner.run_dms()
-w_runner._run_trial(workmate,dms)
-#w_runner.run_otax()
-#w_runner.run_ggsa(prefixed_gates = True)
-toc()
 
-#find pathway
-#import os
-#os.getcwd()
+
  
 
